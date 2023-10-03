ce6d6395fd8392acae545f0ef12511e6f9d9b2ec
Yumicode <75518128+helene-nguyen@users.noreply.github.com>
2023-10-03 22:21:52 +0200

    [frontend] implement theme mode

--- frontend/src/App/Layout/MainLayout/index.tsx
+++ frontend/src/App/Layout/MainLayout/index.tsx
@@ -1,11 +1,14 @@
 //~ Import modules
 import './Style/main.scss';
+import { RootState } from 'Types/GlobalTypes/Store';
+import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 
 //~ Component
 const MainLayout = () => {
+  const theme = useSelector((state: RootState) => state.theme?.default);
   return (
-    <div className="main-layout">
+    <div className={`main-layout theme--${theme}`}>
       <Outlet />
     </div>
   );
