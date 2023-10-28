abad25cde5a1e4712ec1f7ea889756d8a5cf3c89
Yumicode <75518128+helene-nguyen@users.noreply.github.com>
2023-10-28 16:25:43 +0200

    [frontend] handle spinner theme

--- frontend/src/App/Components/Spinner/index.tsx
+++ frontend/src/App/Components/Spinner/index.tsx
@@ -1,9 +1,14 @@
 //~ Imports Modules
 import './Styles/main.scss';
+import './Styles/_theme.scss';
+import { useSelector } from 'react-redux';
+import { RootState } from 'src/Types/GlobalTypes/Store';
 
 //~ Component
 const Spinner = () => {
-  return <div className="loader-drop"></div>;
+  //
+  const theme = useSelector((state: RootState) => state.theme?.default);
+  return <div className={`loader-drop theme--${theme}`}></div>;
 };
 
 export default Spinner;
